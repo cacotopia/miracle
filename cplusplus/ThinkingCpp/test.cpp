1fe904550a600178eb8b4905c1ca253881d1f01d
cacotopia <sinosie7en@gmail.com>
2013-10-29 12:24:28 +0800

    commited on 2013.10.28

--- cplusplus/ThinkingCpp/test.cpp
+++ cplusplus/ThinkingCpp/test.cpp
@@ -4,7 +4,20 @@ using namespace std;
 //{
 
 //}
+
  int main()
 {
     cout<<"Hello World.\n";
+    //函数指针
+    void (*fp)();
+    fp = func;
+    (*fp)();
+    void (*fp2)() = func;
+    (*fp2)();
 }
+void func()
+ {
+     cout<<"func() called ....\n";
+
+ }
+
